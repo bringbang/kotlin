c795b100d014470f688bb41d6da639ccc1ee1f84
Nuttachot Promrit <nuttachot@hotmail.com>
2019-03-04 08:02:59 +0700

    first commit

---
+++ hello.kt
@@ -0,0 +1,4 @@
+fun main() {
+  println("Line: ${readLine()}")
+
+}
---
+++ lab1.kt
@@ -0,0 +1,325 @@
+###1
+fun main() {                        
+	println("Hello, World!")        
+}
+
+###2
+"Hello, World!"
+
+###3
+fun f2() {                        
+	println("")        
+}
+
+###4
+fun f3() {                        
+	println Silpakorn        
+}
+
+###5
+fun f4() {    
+    println(greeting)
+    var greeting = "Hi!"                    
+}
+
+###6
+fun f5() {    
+    greeting = "Hi!"
+    println(greeting)
+}
+
+###7
+fun f6() {    
+    var greeting = "Good morning"
+    greeting = 
+    println(greeting)         
+}
+
+###8
+fun f7() {    
+    var score = 
+    println(score)         
+}
+
+###9
+fun f8() {    
+    var a = 40
+    var b = 2
+    println(a b)         
+}
+
+###10
+fun f9() {    
+    println(s2) //Step3
+    var s2 = s1 + 5 //Step2
+    var s1 = 35 + 2 //Step1
+}
+
+###11
+fun f10() {    
+    message = "Welcome"
+    println(message)
+}
+
+###12
+fun f11() {    
+    var phrase = "Kotlin"
+    
+}
+
+###13
+fun f12() {    
+    //captain marvel
+    
+}
+
+###14
+fun f13() {   
+    peopleSaved = 15
+    birthYear = 1992
+
+    peopleSaved = 35    
+}
+
+###15
+fun f14() {   
+    var name =    
+}
+
+###16
+fun f15() {   
+    var integer =
+    var double =    
+}
+
+###17
+fun f16() {   
+    var integer =
+    var double =    
+}
+
+###18
+fun f17() {   
+    //string
+    var age =
+        
+}
+
+###19
+fun f18() {   
+    var age = 1.998
+    age = 15
+    age = 15.0
+    age = "15"
+}
+
+###20
+fun f19() {   
+    var x = "Alice"
+    var y: = "Bob"
+}
+
+###21
+fun f20() {   
+    //incrementing
+    var score = 4
+    score = 
+    println(score)
+}
+
+###22
+fun f21() {   
+    //incrementing
+    var x = 4
+    x 
+    println(x)
+}
+
+###23
+fun f22() {   
+    var x = 4
+    x  10
+    println(x)
+}
+
+###24 
+fun f23() {
+    //constant
+    threshold = 10
+}
+
+###25
+fun f24() {
+    //T
+    var truth =
+    println(truth)
+}
+
+###26
+fun f25() {
+    //F
+    var truth =
+    println(truth)
+}
+
+###27
+fun f26() {
+    //are great
+    var result = 5 8
+    println(result)
+}
+
+###28
+fun f27() {
+    //are greater than or equql
+    var result = 13 14
+    println(result)
+}
+
+###29
+fun f28() {
+    //equal
+    var x = 3 + 14 - 11
+    var y = 6
+    var result = x  y
+    println(result)
+}
+
+###30
+fun f29() {
+    //change the truth
+    var truth = 
+    println(result)
+}
+
+###31
+fun f30() {
+    //aren't equal
+    var result = 1000 100 
+    println(result)
+}
+
+###32
+fun f31() {
+    //returns true if both of them are true
+    var x = true  flase 
+    println(x)
+}
+
+###33
+fun f32() {
+    //returns true if at least one is true
+    var x = or flase 
+    println(x)
+}
+
+###34
+fun f33() {
+    var a = "Silpakorn"
+    var a = "silpakorn"
+    println(a b)
+}
+
+###35
+fun f34() {
+    //not equal
+    var word= "Silpakorn Sanam Chandra Palace"
+    println(word  "Silpakorn Sanam Chandra")
+}
+
+###36
+fun f35() {
+    //print true
+    var result = true false
+    println(result)
+}
+
+###37
+fun f36() {
+    var condition = true
+    (condition)
+        println(3*4)
+}
+
+###38
+fun f37() {
+    var number = 5
+    if ()
+        println(4*5)
+}
+
+###39
+fun f38() {
+    var condition = false
+    if (condition == false)
+        println(4*5)
+        println("Monte Carlo")
+
+}
+
+###40
+fun f39() {
+    var truth = true
+    if (truth == true) {
+        println("He speaks truth!")
+    } {
+        println("liar! liar!")
+    }
+
+}
+
+###41
+fun f40() {
+    var score = 9
+    if (score >= 10)
+        println("You won!")
+    (score == 5)
+        println("Halfway there!")
+    else
+        println("You can do it")
+}
+
+###42
+fun f41() {
+    var x = true
+    var y = false
+    if (x  y) {
+        println("Woho")
+    }
+}
+
+###43
+fun f42() {
+    var x = false
+    var y = true
+    if ((x == false) (y != true)) {
+        println("Cheer!")
+    }
+}
+
+###44
+fun f43() {
+        println("Less than 10")
+    else
+        println("Greater than or equal to 10")
+    var score = 10
+    if (score < 10)
+}
+
+###45
+fun f44() {
+        println("Less than 10")
+    else
+        println("Greater than or equal to 10")
+    var score = 10
+    if (score < 10)
+}
+
+###46
+fun f45() {
+    var x = true
+    var y = false
+    if ((x==true) and (y true)) {
+        print("Happy day")
+    }
+}
