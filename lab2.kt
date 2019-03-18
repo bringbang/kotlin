4c211ab11fa9cd49270d43a784e64be1c0113b4b
Nuttachot Promrit <nuttachot@hotmail.com>
2019-03-18 07:33:04 +0700

    lab3

--- lab2.kt
+++ lab2.kt
@@ -4,204 +4,208 @@ fun main() {
 }
 
 fun f1() {
-    //val num = 10,20,33,95
-    //println(num.contentToString())
+    val num = arrayOf(10,20,33,95)
+    //println(num)
+    println(num.contentToString())
 }
 
 fun f2() {
-    //val numbers = arrayOf(1, 3, 5, "test")
-    //println(numbers.contentToString())
+    val numbers = arrayOf(1, 3, 5, "test")
+    println(numbers.contentToString())
 }
 
 fun f3() {
-    //val animals =arrayOf("dog", "cat", "mouse")
-    //val cat = animals.get() //get cat
-    //println(cat)
+    val animals =arrayOf("dog", "cat", "mouse")
+    val cat = animals.get(1) //get cat
+    println(cat)
     
 }
 
 fun f4() {
-    //val num = arrayOf(4, 4, 5, 6, 6, 8, 9)
-    //num.(0,3)
-    //println(num.contentToString())    
+    val num = arrayOf(4, 4, 5, 6, 6, 8, 9)
+    num.set(0,3)
+    println(num.contentToString())    
 }
 
 fun f5() {
-    //val animals =arrayOf("dog", "cat", "mouse")
-    //val dog = animals //get dog with []
-    //println(dog)
+    val animals =arrayOf("dog", "cat", "mouse")
+    val dog = animals[0] //get dog with []
+    println(dog)
 }
 
 fun f6() {
-    //val letters = arrayOf('a', 'a', 'c', 'd')
+    val letters = arrayOf('a', 'a', 'c', 'd')
+    letters[1] = 'b'
     //use [] to set fix 'b'
-    //println(letters.contentToString())
+    println(letters.contentToString())
     
 }
 
 fun f7() {
-    //val num = arrayOf(4, 4, 5, 6, 6, 8, 9)
-    //println(num[7])    
+    val num = arrayOf(4, 4, 5, 6, 6, 8, 9)
+    println(num[7])    
     
 }
 
 fun f8() {
-    //val luckyNumber = arrayOf(1, 3, 5, 7, 9, 11)
-    //val numOfElement = luckyNumber.
-    //println(numOfElement)
+    val luckyNumber = arrayOf(1, 3, 5, 7, 9, 11)
+    val numOfElement = luckyNumber.size
+    println(numOfElement)
     
 }
 
 fun f9() {
-    //val luckyNumber = arrayOf(1, 3, 5, 7, 9, 11)
-    //val numOfElement = luckyNumber.size
-    //val lastElement = luckyNumber[]
-    //println(lastElement)
+    val luckyNumber = arrayOf(1, 3, 5, 7, 9, 11)
+    val numOfElement = luckyNumber.size
+    val lastElement = luckyNumber[numOfElement-1]
+    println(lastElement)
     
 }
 
 fun f10() {
-    //val player = arrayOf("Alice", "Bob", "James")
-    //player = "Tim" //set "Alice" to "Tim"
-    //println(player.contentToString())
+    val player = arrayOf("Alice", "Bob", "James")
+    player[0] = "Tim" //set "Alice" to "Tim"
+    println(player.contentToString())
 }
 
 fun f11() {
-    //val player = arrayOf("Tim", "Bob", "James")
-    //val size = player. //find number of element
-    //val last = player[size-1]
-    //println(last)
+    val player = arrayOf("Tim", "Bob", "James")
+    val size = player.size //find number of element
+    val last = player[size-1]
+    println(last)
     
 }
 
 fun f12() {
-    //val letter = 
-    //println(letter) //print a
+    val letter = 'a'
+    println(letter) //print a
     
 }
 
 fun f13() {
-    //val greeting = 
-    //println(greeting) //print Hello
+    val greeting = "Hello"
+    println(greeting) //print Hello
     
 }
 
 fun f14() {
-    //val username = "maximus"
-    //val length = username
-    //if (length < 30) {
-    //    println("Valid username")
-    //}
+    val username = "maximus"
+    val length = username.length
+    if (length < 30) {
+        println("Valid username")
+    }
 }
 
 fun f15() {
-    //val output = "The cat " + 
-    //println(output) //print The cat ate the mouse
+    val output = "The cat " + "ate the mouse"
+    println(output) //print The cat ate the mouse
     
 }
 
 fun f16() {
-    //var question = "Hello,"
-    //question  " where is the cat"
-    //println(question)
+    var question = "Hello,"
+    question += " where is the cat"
+    println(question)
     
 }
 
 fun f17() {
-    //println(shoutMouse)
-    //shoutMouse += '!'
-    //var shoutMouse = "Mouse"
+    var shoutMouse = "Mouse"
+    shoutMouse += '!'
+    println(shoutMouse)
+    
+    
 }
 
 fun f18() {
-    //var phase = "Watch the cat"
-    //phase 
-    //phase += "fish!"
-    //println(phase) //print Watch the cat fish!
+    var phase = "Watch the cat"
+    phase += ' '
+    phase += "fish!"
+    println(phase) //print Watch the cat fish!
 }
 
 fun f19() {
-    //val input = "  cool cat  "
-    //val username = 
-    //println(username) //print cool cat by trim
+    val input = "  cool cat  "
+    val username = input.trim()
+    println(username) //print cool cat by trim
     
 }
 
 fun f20() {
-    //val livesLeft = 9
-    //val output = " lives left for catman."
-    //println(output) //use ${val_name} to print 9 lives left for catman.
+    val livesLeft = 9
+    val output = "${livesLeft} lives left for catman."
+    println(output) //use ${val_name} to print 9 lives left for catman.
     
 }
 
 fun f21() {
-    //val output = "She said "I love coding\", after building her first app."
-    //println(output) //print She said "I love coding", after building her first app.
+    val output = "She said \"I love coding\", after building her first app."
+    println(output) //print She said "I love coding", after building her first app.
     
 }
 
 fun f22() {
-    //val accountNumber = 1234
-    //val output = "Account number: {accountNumber}"
-    //println(output)
+    val accountNumber = 1234
+    val output = "Account number: ${accountNumber}"
+    println(output)
     
 }
 
 fun f23() {
-    //repeat () { //
-    //    println("Time for loop!")
-    //}
+    repeat (4) { //
+        println("Time for loop!")
+    }
     
 }
 
 
 fun f24() {
-    //repeat (2)
-    //    println("No, Polly")
-    
+    repeat (2) {
+        println("No, Polly")
+    }
 }
 
 fun f25() {
-    //{
-    //    println("Captain marvel is great!")
-    //}
+    repeat (4) {
+        println("Captain marvel is great!")
+    }
     
 }
 
 fun f26() {
-    //counter = 0
-    //repeat(4) {
-        
-    //    println()
+    var counter = 0
+    repeat(4) {
+        counter+=1
+        println(counter)
         //print 1
         //      2
         //      3
         //      4
-    //}
+    }
     
 }
 
 fun f27() {
-    //var index = 0
-    // (index < 6) { //use while
-    //    index += 1
-    //    println(index)
-    //}
+    var index = 0
+    while (index < 6) { //use while
+        index += 1
+        println(index)
+    }
     
 }
 
 fun f28() {
-    //val names = arrayOf("Tom", "Sam", "Alex")
-    // (n  names) {
-    //    println(n)
-    //}
+    val names = arrayOf("Tom", "Sam", "Alex")
+    for (n in names) {
+        println(n)
+    }
     //use for in
     
 }
 
 fun f29() {
 
-    for (number in ) {
+    for (number in 2..5) {
         println(number)
     }
     //2..5
---
+++ lab3.kt
@@ -0,0 +1,170 @@
+/*
+helloWorld() {
+    println("Hello world")
+}
+ 
+fun printMessage(text:) {
+    println(text)
+}
+
+fun printAge(name:String) { //age:Int
+    println("${name} is ${age} years old.")
+}
+
+fun giveMeFive():Int {
+     5//return 5
+}
+
+fun getTwoTimes(number:Int) {
+    return 2*number
+}
+
+fun sumFive() {
+    val sum = 6 + //plus with function giveMeFive()
+    println(sum)
+}
+
+
+fun displayOneTo(number Int) {
+    for(index in 1..number){
+        println("${index}")
+    }
+}
+
+fun isValid(username:String):Boolean {
+     (username.length < 30)
+}
+
+fun arrayList1(){
+    val friends = ("sam", "Amy", "Trudy") //arrayListOf
+    println(friends)
+}
+
+fun arrayList2(){
+    val friends = arrayListOf("sam", "Amy", "Trudy")
+    val firstFriend = friends.get() //get first Friend
+    println(firstFriend)
+}
+
+fun arrayList3(){
+    val friends = arrayListOf("sam", "Amy", "Trudy")
+    val thirdFriend = friends[] //get third Friend
+    println(thirdFriend)
+}
+
+fun arrayList4(){
+    val friends = arrayListOf("sam", "Amy", "Trudy")
+     =  //change Trudy to Ron
+    println(friends)
+}
+
+fun arrayList5(){
+    val friends = arrayListOf("sam")
+    friends.("Alice") //use add method
+    println(friends)
+}
+
+fun arrayList6(){
+    val places = arrayListOf("sam2nd", "3rd")
+    places.add(, "1st") //add 1s to the beginnind
+    println(places)
+}
+
+fun arrayList7(){
+    val colors =arrayListOf("red", "cat", "blue", "green")
+    colors.() //remove at index 1
+    println(colors)
+}
+
+fun arrayList8(){
+    val schedule = arrayListOf("play", "clean", "run outside")
+    schedule.() //clear array list
+    println(schedule)
+}
+
+fun arrayList9(){
+    val numbers =arrayListOf() //use <> to specify type
+    numbers.add(1)
+    println(numbers)
+}
+
+class Person 
+    var name:String = ""
+    var age:Int = 0
+
+    fun greet(){
+        println("Hi, I am a person!")
+    }
+
+fun getAge(){
+    val jim = Person()
+    println("Age: ${jim.}")
+
+}
+
+fun setPerson(){
+    val jim = Person()
+    jim. = "Jim"
+    jim. = 18
+    println("Name: ${jim.name}")
+    println("Age: ${jim.age}")
+
+}
+
+class Person2(var age:Int = 0) {
+
+}
+
+class Person3(var name:String = "") {
+    fun introduce(){
+        println("I am ${}!")
+    }
+
+}
+
+fun printName(){
+    val bob = Person3("Bob")
+    bob.introduce()
+}
+
+class Person4(var fName:String = "", var lName:String = "") {
+    var fullName:String = ""
+    init {
+        fullName = fName + " " + lName
+    }
+
+}
+
+fun initializer(){
+    val indy = Person4("Indiana", "Jones")
+    println(indy.fullName)
+}
+
+
+fun passArgument(){
+    val sam = Person4(fName = "Sam", lName = "Sun")
+    println(sam.fullName)
+}
+
+class Person5 {
+    var feeling = ""
+    fun updateStatus(feeling:String) {
+        .feeling = feeling //use this
+    }
+}
+
+fun setfeeling() {
+    val bob = Person5()
+    bob.updateStatus("fantastic")
+    println(bob.feeling)
+}
+
+fun inputNumber() {
+    var number = readLine()
+    println("Your number is $number")
+}
+*/
+
+fun main() {
+    
+}
